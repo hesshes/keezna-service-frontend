8a2a60c36f4670a0fac9e5349938a77564d408dc
taewoo kim <hesshes@naver.com>
2025-08-24 16:12:34 +0900

    api ...

--- src/compoonents/image/Image.tsx
+++ src/compoonents/image/Image.tsx
@@ -1,17 +1,38 @@
+import { useState } from "react";
 import "../../assets/css/comp/image.scss";
 const Image = (): JSX.Element => {
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const files = e.currentTarget.files;
+        if (files && files.length > 0) {
+            setFile(files[0]);
+        } else {
+            setFile(null);
+        }
+    };
+
+    const handleImageConvert = (): void => {
+        console.log("test");
+    };
     return (
         <>
             <div id="image__wrapper">
-                <div>
+                <div className="image__upload">
                     <label htmlFor="imageUpload" className="custom-file-upload">
                         이미지 업로드
                     </label>
-                    <input type="file" id="imageUpload" name="image" />
+                    <span>
+                        {file == null ? "선택된 파일이 없습니다" : file.name}
+                    </span>
+                    <input
+                        type="file"
+                        id="imageUpload"
+                        name="image"
+                        onChange={handleFileChange}
+                    />
                 </div>
-                <div>
-                    <h1>결과화면 보여줄거</h1>
-                </div>
+                <button onClick={handleImageConvert}>전송</button>
             </div>
         </>
     );
